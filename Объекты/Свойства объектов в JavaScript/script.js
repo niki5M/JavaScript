0c7b2d5be2da8937631915b0105a0def6d0ccc05
niki5M <meshkova.n.i.22@gmail.com>
2023-10-04 22:16:04 +0300

    javas

---
+++ Объекты/Свойства объектов в JavaScript/script.js
@@ -0,0 +1,2 @@
+let date = {year: 2023, month: 10, day: 4};
+console.log(date.year,'-',date.month,'-',date.day);
---
+++ Объекты/Строковые ключи объектов в JavaScript/script.js
@@ -0,0 +1,2 @@
+let user = {name: 'Мешкова', surname: 'Нина', patronymic: 'Игоревна'};
+console.log(user['name'],  user['surname'],  user['patronymic'])
